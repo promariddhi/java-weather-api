de4885ca7e58ed96f313ed32e132e4c072f21d19
promariddhi <114817746+promariddhi@users.noreply.github.com>
2026-06-27 20:17:58 +0530

    chore: setup spring boot environment

---
+++ src/main/java/com/proma/java_weather_api/JavaWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.proma.java_weather_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proma/java_weather_api/JavaWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proma.java_weather_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
